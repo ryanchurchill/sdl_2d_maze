d7b0c181f42ec59f8a3e8fab359080d1e33f974c
Ryan Churchill <ryan.churchill@gmail.com>
2021-04-21 20:26:10 -0700

    attempt at geometry rendering (unsuccessful)

--- sdl_2d_maze/02_getting_an_image_on_the_screen.cpp
+++ sdl_2d_maze/02_getting_an_image_on_the_screen.cpp
@@ -4,6 +4,7 @@ and may not be redistributed without written permission.*/
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <string>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -18,14 +19,23 @@ bool loadMedia();
 //Frees media and shuts down SDL
 void close();
 
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-	
 //The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
+//SDL_Surface* gScreenSurface = NULL;
 
 //The image we will load and show on the screen
-SDL_Surface* gHelloWorld = NULL;
+//SDL_Surface* gHelloWorld = NULL;
+
+//Loads individual image as texture
+//SDL_Texture* loadTexture(std::string path);
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Current displayed texture
+SDL_Texture* gTexture = NULL;
 
 bool init()
 {
@@ -49,8 +59,26 @@ bool init()
 		}
 		else
 		{
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface( gWindow );
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				//Initialize PNG loading
+				/*int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}*/
+			}
 		}
 	}
 
@@ -63,12 +91,12 @@ bool loadMedia()
 	bool success = true;
 
 	//Load splash image
-	gHelloWorld = SDL_LoadBMP( "hello_world.bmp" );
+	/*gHelloWorld = SDL_LoadBMP( "hello_world.bmp" );
 	if( gHelloWorld == NULL )
 	{
 		printf( "Unable to load image %s! SDL Error: %s\n", "hello_world.bmp", SDL_GetError() );
 		success = false;
-	}
+	}*/
 
 	return success;
 }
@@ -76,8 +104,8 @@ bool loadMedia()
 void close()
 {
 	//Deallocate surface
-	SDL_FreeSurface( gHelloWorld );
-	gHelloWorld = NULL;
+	//SDL_FreeSurface( gHelloWorld );
+	//gHelloWorld = NULL;
 
 	//Destroy window
 	SDL_DestroyWindow( gWindow );
@@ -109,6 +137,7 @@ int main( int argc, char* args[] )
 			// Event Handler
 			SDL_Event e;
 
+			// Main Loop
 			while (!quit) {
 				//Handle events on queue
 				while (SDL_PollEvent(&e) != 0)
@@ -126,7 +155,21 @@ int main( int argc, char* args[] )
 				}
 
 				//Apply the image
-				SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+				//SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+				//Clear screen
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				SDL_RenderClear(gRenderer);
+
+				//Render red filled quad
+				SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+				SDL_RenderFillRect(gRenderer, &fillRect);
+
+				//Render green outlined quad
+				SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+				SDL_RenderDrawRect(gRenderer, &outlineRect);
 
 				//Update the surface
 				SDL_UpdateWindowSurface(gWindow);
